1c50589bbb7c3881edbe3d534294ac3d2d69f915
Bhavya2995 <bhavya@excellencetechnologies.in>
2018-03-21 19:20:26 +0530

    design changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,9 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
-  return (<div>MainPage
-  <Link to = "/">Logout</Link>
-  </div>);
+  const styleHeader = {
+    border: "1px solid",
+    backgroundColor: "#121926",
+    color: "white",
+    height: "7vh"
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="navbar-brand" href="#">
+          PollingApp
+        </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item ">
+              <Link className="nav-link" to="/">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default MainPage;
